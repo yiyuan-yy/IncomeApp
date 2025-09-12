96d6d33aa486baa80c13a427a3b8b73ceee7f7f6
yiyuan <euniceyyhu@gmail.com>
2025-09-12 16:57:02 +0800

    added the balance card in home view

--- IncomeApp/Views/HomeView.swift
+++ IncomeApp/Views/HomeView.swift
@@ -10,13 +10,65 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                settingButton
+                titleView
+                overviewCard
+            }
+            .padding(.horizontal)
+            
+            Spacer()
         }
-        .padding()
     }
+    
+    private var overviewCard: some View{
+        VStack (spacing: 10) {
+            HStack {
+                Text("BALANCE")
+                Spacer()
+            }
+            HStack {
+                Text("ToTal Number $")
+                    .font(.system(size: 40))
+                Spacer()
+            }
+            HStack {
+                VStack{
+                    Text("EXPENSE")
+                    Text("US$0.00")
+                }
+                VStack{
+                    Text("Income")
+                    Text("US$0.00")
+                }
+                Spacer()
+            }
+        }
+        .foregroundStyle(.white)
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color.primaryTheme)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .shadow(radius: 8)
+    }
+    
+    private var titleView: some View{
+        HStack {
+            Text("Income")
+                .font(.system(size: 30, weight: .heavy))
+            Spacer()
+            
+        }
+    }
+    
+    private var settingButton: some View{
+        HStack {
+            Spacer()
+            Image(systemName: "gearshape.fill")
+                .font(.system(size: 20))
+        }
+    }
+    
 }
 
 #Preview {
