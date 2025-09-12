7fcfe51bcd90b26bb36df6687aa87417246b6e25
yiyuan <euniceyyhu@gmail.com>
2025-09-12 14:18:04 +0800

    Initial Commit

---
+++ IncomeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IncomeApp
+//
+//  Created by yiyuan hu on 9/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IncomeApp/IncomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeAppApp.swift
+//  IncomeApp
+//
+//  Created by yiyuan hu on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
